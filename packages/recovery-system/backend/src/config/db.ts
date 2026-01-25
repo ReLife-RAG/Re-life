29430c4e2fe59273b0e9f7bda10beae5c8958a56
K.K Ranathunga <kashyaparanathunga2003@gmail.com>
2026-01-25 21:37:34 +0530

    make the database configuration

--- packages/recovery-system/backend/src/config/db.ts
+++ packages/recovery-system/backend/src/config/db.ts
@@ -1,1 +1,13 @@
 // Database, environment configs
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI || "");
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch(error){
+        console.error(`Error: ${error}`);
+        process.exit(1);
+    }
+}
+export default connectDB;
